76c4411269bd612f9e8b9e507c0eb30897b99cec
zehguilherme <36301054+zehguilherme@users.noreply.github.com>
2020-02-22 15:49:10 -0300

    Identation update and add function   collisionDetect() {

--- developer-mozilla/pratica-objetos/js/main.js
+++ developer-mozilla/pratica-objetos/js/main.js
@@ -1,18 +1,19 @@
+const paragraph = document.querySelector('p');
 
 // setup canvas
 
 const canvas = document.querySelector('canvas');
 
 // ctx representa a área de desenho em tela
-const ctx = canvas.getContext('2d');  //pega o contexto atual a ser usado em tela (2d)
+const ctx = canvas.getContext('2d'); //pega o contexto atual a ser usado em tela (2d)
 
 // Igualar altura e largura da viewport do navegador com a do canvas
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
 // função para gerar um número aleatório no intervalo entre os 2
-function random(min,max) {
-  const num = Math.floor(Math.random()*(max-min)) + min;
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min)) + min;
   return num;
 }
 
@@ -21,9 +22,9 @@ class Shape {
   constructor(x, y, velX, velY) {
     this.x = x;
     this.y = y;
-    this.velX = velX;      //velocidade eixo X
-    this.velY = velY;      //velocidade eixo Y
-    this.exists = exists;  //rastreia se as bolas existem (se não foram eliminadas pela bola maligna)
+    this.velX = velX; //velocidade eixo X
+    this.velY = velY; //velocidade eixo Y
+    this.exists = exists; //rastreia se as bolas existem (se não foram eliminadas pela bola maligna)
   }
 };
 
@@ -33,12 +34,12 @@ class Ball extends Shape {
     super(x, y, velX, velY, exists);
 
     this.color = color;
-    this.size = size;      //raio em pixels
+    this.size = size; //raio em pixels
   }
 
   // Método para desenhar
   draw() {
-    ctx.beginPath();            //desenhar uma forma
+    ctx.beginPath(); //desenhar uma forma
     ctx.fillStyle = this.color; //fillStyle: define a cor da forma
     //arc(): traçar o formato de um arco
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI, true); //parâmetros: posição x e y do centro do arco; raio do arco (size); 2 últimos: nº inicial e final de graus em volta do círculo em que o arco é desenhado ( 0º e 2 * PI(eq 360º) )
@@ -51,25 +52,25 @@ class Ball extends Shape {
 
     // Verf se coord. x (centro da bola) é maior que largura da tela (bola está saindo da borda direita)
     // size da bola é incluído no cálculo
-    if((this.x + this.size) >= width) {
+    if ((this.x + this.size) >= width) {
       this.velX = -(this.velX);
     }
-    
+
     // Verf se coord. x (centro da bola) é menor que 0 (bola está saindo da borda esquerda)
     // size da bola é incluído no cálculo
-    if((this.x - this.size) <= 0) {
+    if ((this.x - this.size) <= 0) {
       this.velX = -(this.velX);
     }
-    
+
     // Verf se coord. y (centro da bola) é maior que a altura da tela (bola está saindo da borda inferior)
     // size da bola é incluído no cálculo
-    if((this.y + this.size) >= height) {
+    if ((this.y + this.size) >= height) {
       this.velY = -(this.velY);
     }
-    
+
     // Verf se coord. y (centro da bola) é menor que 0 (bola está saindo da borda superior)
     // size da bola é incluído no cálculo
-    if((this.y - this.size) <= 0) {
+    if ((this.y - this.size) <= 0) {
       this.velY = -(this.velY);
     }
 
@@ -81,17 +82,17 @@ class Ball extends Shape {
   // Detecção de colisão
   collisionDetect() {
     // Percorrer array de bolas
-    for(let j = 0; j < balls.length; j++) {
-      if(!(this === balls[j])) {  //verifica se a bola atual do loop não é a mesma que está sendo verificada no momento (collisionDetect())
+    for (let j = 0; j < balls.length; j++) {
+      if (!(this === balls[j])) { //verifica se a bola atual do loop não é a mesma que está sendo verificada no momento (collisionDetect())
         // verifica se alguma das áreas dos 2 círculos se sobrepõem
         const dx = this.x - balls[j].x;
         const dy = this.y - balls[j].y;
         const distance = Math.sqrt(dx * dx + dy * dy);
 
         // se detectar colisão
-        if(distance < this.size + balls[j].size) {
+        if (distance < this.size + balls[j].size) {
           // muda-se a cor de ambos os círculos para uma cor aleatória
-          balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+          balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
         }
       }
     }
@@ -102,15 +103,15 @@ class Ball extends Shape {
 class EvilCircle extends Shape {
   constructor(x, y, velX = 20, velY = 20, exists) {
     super(x, y, velX, velY, exists);
-    
+
     this.color = 'white';
     this.size = 10;
   }
 
   draw() {
-    ctx.beginPath();              //desenhar uma forma
+    ctx.beginPath(); //desenhar uma forma
     ctx.strokeStyle = this.color; //fillStyle: define apenas um traço externo com a cor
-    ctx.lineWidth(3);              //torna o traço um pouco mais espesso
+    ctx.lineWidth(3); //torna o traço um pouco mais espesso
     //arc(): traçar o formato de um arco
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI, true); //parâmetros: posição x e y do centro do arco; raio do arco (size); 2 últimos: nº inicial e final de graus em volta do círculo em que o arco é desenhado ( 0º e 2 * PI(eq 360º) )
     ctx.stroke(); //termina de "desenhar" o que começou em beginPath() e faz o traço com a cor especifica em strokeStyle acima
@@ -118,25 +119,25 @@ class EvilCircle extends Shape {
 
   // Fará a mesma coisa que a primeira parte da função update() de Ball()
   checkBounds() {
-    if((this.x + this.size) >= width) {
+    if ((this.x + this.size) >= width) {
       this.x = -(this.x);
     }
-    
+
     // Verf se coord. x (centro da bola) é menor que 0 (bola está saindo da borda esquerda)
     // size da bola é incluído no cálculo
-    if((this.x - this.size) <= 0) {
+    if ((this.x - this.size) <= 0) {
       this.x = -(this.x);
     }
-    
+
     // Verf se coord. y (centro da bola) é maior que a altura da tela (bola está saindo da borda inferior)
     // size da bola é incluído no cálculo
-    if((this.y + this.size) >= height) {
+    if ((this.y + this.size) >= height) {
       this.y = -(this.y);
     }
-    
+
     // Verf se coord. y (centro da bola) é menor que 0 (bola está saindo da borda superior)
     // size da bola é incluído no cálculo
-    if((this.y - this.size) <= 0) {
+    if ((this.y - this.size) <= 0) {
       this.y = -(this.y);
     }
   }
@@ -145,50 +146,83 @@ class EvilCircle extends Shape {
   setControls() {
     let _this = this;
 
-    onkeydown = function(e) {
-      if(e.keyCode === 65) {
+    // 65, 68, 87, 83: códigos que representam determinadas teclas
+    //assim que uma tecla é pressionada, a prop. keyCode é consultada pra ver qual tecla é
+    onkeydown = function (e) {
+      if (e.keyCode === 65) { //tecla A
         _this.x -= _this.velX;
-      } else if(e.keyCode === 68) {
+      } else if (e.keyCode === 68) { //tecla D
         _this.x += _this.velX;
-      } else if(e.keyCode === 87) {
+      } else if (e.keyCode === 87) { //tecla W
         _this.y -= _this.velY;
-      } else if(e.keyCode === 83) {
+      } else if (e.keyCode === 83) { //tecla S
         _this += _this.velY;
+      }
+    }
+  }
+
+  // Irá agir de forma muito semelhante ao método collisionDetect() do Ball()
+  collisionDetect() {
+    // Percorrer array de bolas
+    for (let j = 0; j < balls.length; j++) {
+      if ((this.exists == true)) { //verifica se a bola atual existe
+        // verifica se alguma das áreas dos 2 círculos se sobrepõem
+        const dx = this.x - balls[j].x;
+        const dy = this.y - balls[j].y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        // se detectar colisão
+        if (distance < this.size + balls[j].size) {
+          // muda-se a cor de ambos os círculos para uma cor aleatória
+          balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+        }
       }
     }
   }
 };
 
 // Animar bola
-let balls = [];  //vetor para armazenar todas as bolas
+let balls = []; //vetor para armazenar todas as bolas
 
 // Valores aleatórios são gerados e adicionados no final do array de bolas (apenas quando tiver menos que 25 bolas)
 // Quando há 25 bolas na tela, não aparecem mais
-while(balls.length < 25) {
+while (balls.length < 25) {
   const size = random(10, 20);
   let ball = new Ball(
     // a posição da bola sempre é desenhada a pelo menos uma largura da borda da tela para evitar erros de desenho
-    random(0 + size,width - size),
-    random(0 + size,height - size),
-    random(-7,7),
-    random(-7,7),
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
     exists = true,
-    'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+    'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
     size
   );
   balls.push(ball);
 }
 
 // Função loop de animação - atualizar as informações no programa e renderizar a visualização resultado em cada quadro da animação
-function loop(){
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';  //cor de preenchimento da tela
-  ctx.fillRect(0, 0, width, height);      //desenha retangulo da mesma cor na largura, altura da tela
+function loop() {
+  let evilcircle = new EvilCircle(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    exists = true
+  );
+
+  evilcircle.setControls();
+
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)'; //cor de preenchimento da tela
+  ctx.fillRect(0, 0, width, height); //desenha retangulo da mesma cor na largura, altura da tela
 
   // Percorre todo o array de bolas
-  for(let i = 0; i < balls.length; i++) {
-    balls[i].draw();             //desenha cada uma das bolas
-    balls[i].update();           //atualiza a posição e velocidade no tempo para o próximo quadro
-    balls[i].collisionDetect();  //detecção de colisão
+  for (let i = 0; i < balls.length; i++) {
+    // Verifica se a bola atual existe
+    if (exists == true) {
+      balls[i].draw(); //desenha cada uma das bolas
+      balls[i].update(); //atualiza a posição e velocidade no tempo para o próximo quadro
+      balls[i].collisionDetect(); //detecção de colisão
+      balls[i].checkBounds();
+    }
   }
 
   // cria uma animação suave ao executar a função 'loop' um número determinado de vezes por segundo (recursivamente)
