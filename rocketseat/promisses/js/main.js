6ccebe342a9a84740e1348796cc973049a6a8f59
zehguilherme <36301054+zehguilherme@users.noreply.github.com>
2020-03-04 15:41:58 -0300

    Use of axios instead of XMLHttpRequest

--- rocketseat/promisses/js/main.js
+++ rocketseat/promisses/js/main.js
@@ -1,34 +1,12 @@
-const myPromisse = function() {
-  //resolve: retorna quando foi obtido sucesso
-  //reject: retorna quando não foi obtido sucesso
-  return new Promise(function(resolve, reject) {
-    let xhr = new XMLHttpRequest();
+axios
+  .get("http://api.github.com/users/zehguilherme")
 
-    xhr.open("GET", "https://api.github.com/users/zehguilherme");
-    xhr.send(null);
+  //sucesso: resolve.then()
+  .then(function(response) {
+    console.log(response);
+  })
 
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4) {
-        // retorna o resultado da promisse
-
-        if (xhr.status === 200) {
-          // sucesso
-          resolve(JSON.parse(xhr.responseText));
-        } else {
-          //erro
-          reject("Erro na requisição");
-        }
-      }
-    };
+  //erro: reject.catch()
+  .catch(function(error) {
+    console.warn(error);
   });
-};
-
-//sucesso: resolve.then()
-myPromisse().then(function(response) {
-  console.log(response);
-});
-
-//erro: reject.catch()
-myPromisse().catch(function(error) {
-  console.warn(error);
-});
