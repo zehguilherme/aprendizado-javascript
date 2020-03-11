f30fc1e6ee39d98498329205aa3d485648967095
zehguilherme <36301054+zehguilherme@users.noreply.github.com>
2020-03-10 21:22:37 -0300

    Update indentation and variables

--- rocketseat/exercicios/n2-n3/js/main.js
+++ rocketseat/exercicios/n2-n3/js/main.js
@@ -1,23 +1,27 @@
-const github_username = document.querySelector("input#user"); //input
+/* eslint-disable no-undef */
+/* eslint-disable camelcase */
+/* eslint-disable no-unused-vars */
+/* eslint-disable space-before-function-paren */
+
+const github_username = document.querySelector('input#user') // input
 
 // Botão adicionar
 const btAdd = document
-  .querySelector("button.add")
-  .addEventListener("click", () => {
-    let loadingElement = document.createElement("li");
-    loadingElement.textContent = "Carregando...";
+  .querySelector('button.add')
+  .addEventListener('click', () => {
+    const loadingElement = document.createElement('li')
+    loadingElement.textContent = 'Carregando...'
 
-    let brElement = document.createElement("br");
+    const brElement = document.createElement('br')
 
-    document.querySelector("ul.list").appendChild(loadingElement); //coloca a linha "Carregando" na ul
-    document.querySelector("ul.list").before(brElement); //coloca um br antes da ul
+    document.querySelector('ul.list').appendChild(loadingElement) // coloca a linha "Carregando" na ul
+    document.querySelector('ul.list').before(brElement) // coloca um br antes da ul
 
-    axios
-      .get(`https://api.github.com/users/${github_username.value}/repos`)
+    axios.get(`https://api.github.com/users/${github_username.value}/repos`)
 
       .then(response => {
-        document.querySelector("ul.list").removeChild(loadingElement);
-        document.querySelector("div.form").removeChild(brElement);
+        document.querySelector('ul.list').removeChild(loadingElement) // remove "Carregando" ao entrar no response
+        document.querySelector('div.form').removeChild(brElement)
 
         // Verifica se está inserindo os mesmo repositórios mais de uma vez (mesmo usuário)
         // if (
@@ -33,62 +37,60 @@ const btAdd = document
 
         // Percorre o vetor com as informações sobre os repositórios vindo do response
         for (let i = 0; i < response.data.length; i++) {
-          let itemElement = document.createElement("li"); //criação de uma linha
-          let linkElement = document.createElement("a"); //link
-          let linkAttribute = document.createAttribute("href"); //href
+          const itemElement = document.createElement('li') // criação de uma linha
+          const linkElement = document.createElement('a') // link
+          const linkAttribute = document.createAttribute('href') // href
 
-          linkAttribute.value = response.data[i].html_url; //atribui a URL dos repositorios a cada href
-          linkElement.setAttributeNode(linkAttribute); //coloca os href dentro do link
-          linkElement.textContent = response.data[i].name; //Pega os nomes dos repositórios e coloca no nó de texto do link ao lado do href
+          linkAttribute.value = response.data[i].html_url // atribui a URL dos repositorios a cada href
+          linkElement.setAttributeNode(linkAttribute) // coloca os href dentro do link
+          linkElement.textContent = response.data[i].name // Pega os nomes dos repositórios e coloca no nó de texto do link ao lado do href
 
-          document.querySelector("ul.list").appendChild(itemElement); //coloca as linhas na ul
+          document.querySelector('ul.list').appendChild(itemElement) // coloca as linhas na ul
 
-          itemElement.appendChild(linkElement); //coloca os links nas linhas
+          itemElement.appendChild(linkElement) // coloca os links nas linhas
         }
       })
 
-      .catch(error => {
-        alert(
-          `Erro na requisição! \nUsuário ${github_username.value} não encontrado!`
-        );
+      .catch(() => {
+        alert('Erro na requisição!')
 
-        clearInput();
-      });
-  });
+        clearInput()
+      })
+  })
 
 // Botão limpar
 const btClear = document
-  .querySelector("button.clear")
-  .addEventListener("click", () => {
-    let lines = document.querySelector("li");
+  .querySelector('button.clear')
+  .addEventListener('click', () => {
+    const lines = document.querySelector('li')
 
     // Se as linhas existem, são apagadas
     if (lines) {
-      clearLines();
+      clearLines()
     } else {
-      alert("Nenhum repositório encontrado para ser apagado");
+      alert('Nenhum repositório encontrado para ser apagado')
     }
 
     // Verifica se o input está vazio
-    if (github_username.value === "") {
-      alert("Não foi encontrado nada digitado no campo");
+    if (github_username.value === '') {
+      alert('Não foi encontrado nada digitado no campo')
     } else {
-      clearInput();
+      clearInput()
     }
-  });
+  })
 
 // Limpa o input e coloca o foco nele
 function clearInput() {
-  github_username.value = "";
-  github_username.focus();
+  github_username.value = ''
+  github_username.focus()
 }
 
 // Limpa as linhas em que estão os repositórios
 function clearLines() {
-  let list = document.querySelector("ul.list");
+  const list = document.querySelector('ul.list')
 
   // Enquanto a ul tiver alguma li (filha de ul)
   while (list.firstChild) {
-    list.removeChild(list.firstChild);
+    list.removeChild(list.firstChild)
   }
 }
